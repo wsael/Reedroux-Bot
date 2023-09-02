4eec8272d2aaebcd0d424b043f6ac3fea3878cce
Dawg | Rem-Chan <115357133+dawgcodes@users.noreply.github.com>
2023-09-02 20:14:53 +0200

    Update and rename index.js to index.ts

--- index.js
+++
@@ -1,8 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const config = require('./config.json');
-
-const manager = new ShardingManager('./muzox.js', { token: config.token });
-
-manager.on('shardCreate', shard => console.log(`shard ${shard.id} launched`));
-
-manager.spawn();
