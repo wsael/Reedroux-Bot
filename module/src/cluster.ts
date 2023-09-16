0204d9bb8fe6ab67abe42e459902f681c1b88457
wsael <134006961+wsael@users.noreply.github.com>
2023-09-16 06:24:11 +0200

    Create cluster.ts

---
+++ module/src/cluster.ts
@@ -0,0 +1,8 @@
+import { BaseDiscordClient } from "./BaseDiscordClient";
+import config from "../config";
+
+const client = new BaseDiscordClient(config);
+
+client.listenners(config.debug);
+client.init();
+
