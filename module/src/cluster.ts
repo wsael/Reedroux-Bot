e5113937f886e99e8cf57dd3192f94e7115fc2e2
David Black <ianblackdavid@gmail.com>
2024-02-04 20:26:08 +0100

    Delete module/src directory

--- module/src/cluster.ts
+++
@@ -1,8 +0,0 @@
-import { BaseDiscordClient } from "./BaseDiscordClient";
-import config from "../config";
-
-const client = new BaseDiscordClient(config);
-
-client.listenners(config.debug);
-client.init();
-
